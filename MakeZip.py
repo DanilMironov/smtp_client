58399562839419347073fba1fb04624ae3797cc1
DanilMironov <41448941+DanilMironov@users.noreply.github.com>
2018-11-20 00:55:20 +0500

    Add files via upload

--- MakeZip.py
+++ MakeZip.py
@@ -11,21 +11,22 @@ class Ziper:
             with zipfile.ZipFile(zip_path, 'w') as new_zip:
                 for folder, subfolder, files in os.walk(address):
                     for file in files:
-                        new_zip.write(os.path.join(folder, file), compress_type=zipfile.ZIP_DEFLATED)
+                        new_zip.write(os.path.join(folder, file),
+                                      compress_type=zipfile.ZIP_DEFLATED)
         else:
             with zipfile.ZipFile(zip_path, 'w') as new_zip:
                 new_zip.write(address, compress_type=zipfile.ZIP_DEFLATED)
 
     @staticmethod
     def get_zip_path(address):
-        suffix = re.search(r'\\?([ _0-9а-яА-Я\w]+)([^\\]\w+)?$', address).group(1)
-        # zip_path = r'C:\Users\я\Desktop\{0}.zip'.format(suffix)
+        suffix = re.search(r'\\?([ _0-9а-яА-Я\w]+)([^\\]\w+)?$',
+                           address).group(1)
         try:
-            zip_folder = re.search(r'(.+)(\\([ _0-9а-яА-Я\w]+)(.\w+)?$)', address)[1] + '\\'
+            zip_folder = re.search(r'(.+)(\\([ _0-9а-яА-Я\w]+)(.\w+)?$)',
+                                   address)[1] + '\\'
         except TypeError:
             zip_folder = ''
         zip_path = '{0}{1}.zip'.format(zip_folder, suffix)
         is_folder = address.endswith(suffix)
         Ziper.make_zip(address, zip_path, is_folder)
         return zip_path
-
